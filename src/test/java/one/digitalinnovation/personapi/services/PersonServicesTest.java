ceace15990e465cfde9cb89db7a053802ad66a34
Victor Hugo Landin <victor.landin@estudante.ufla.br>
2021-06-26 14:29:39 -0300

    adiciona teste unitário para criação de Person com cpf duplicado

--- src/test/java/one/digitalinnovation/personapi/services/PersonServicesTest.java
+++ src/test/java/one/digitalinnovation/personapi/services/PersonServicesTest.java
@@ -1,5 +1,7 @@
 package one.digitalinnovation.personapi.services;
 
+import one.digitalinnovation.personapi.exceptions.PersonAlreadyRegisteredException;
+import one.digitalinnovation.personapi.mappers.PersonMapper;
 import one.digitalinnovation.personapi.models.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.models.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.models.entity.Person;
@@ -10,9 +12,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static one.digitalinnovation.personapi.utils.PersonUtils.createFakeDTO;
 import static one.digitalinnovation.personapi.utils.PersonUtils.createFakeEntity;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -22,11 +27,13 @@ public class PersonServicesTest {
     @Mock
     private PersonRepository personRepository;
 
+    private PersonMapper personMapper = PersonMapper.INSTANCE;
+
     @InjectMocks
     private PersonServices personServices;
 
     @Test
-    void testGivenPersonDTOThenReturnSuccessSavedMessage() {
+    void testGivenPersonDTOThenReturnSuccessSavedMessage() throws PersonAlreadyRegisteredException {
 
         PersonDTO personDTO = createFakeDTO();
         Person expectedSavePerson = createFakeEntity();
@@ -37,6 +44,18 @@ public class PersonServicesTest {
         MessageResponseDTO successMessage = personServices.createPerson(personDTO);
 
         assertEquals(expectedSucessMessage, successMessage);
+
+    }
+
+    @Test
+    void testGivenPersonDTOWithRegisteredCPFThenExceptionShouldBeThrow() {
+
+        PersonDTO personDTO = createFakeDTO();
+        Person duplicatedPerson = personMapper.toModel(personDTO);
+
+        when(personRepository.findFirstByCpf(personDTO.getCpf())).thenReturn(Optional.of(duplicatedPerson));
+
+        assertThrows(PersonAlreadyRegisteredException.class, () -> personServices.createPerson(personDTO));
 
     }
 
